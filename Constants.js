9e9ea672afff642472d52680ee427865e4ed40e8
Deepthi-Ramesh <deepthimr1411@gmail.com>
2024-05-24 17:53:22 +0530

    hosturl commited

--- Constants.js
+++ Constants.js
@@ -1,5 +1,5 @@
 module.exports = {
     // HOST_URL : 'https://prem-todos.herokuapp.com'
-    HOST_URL : 'https://rentify-frontend-coral.vercel.app/'
+    HOST_URL : 'https://rentify-backend-six.vercel.app/'
     // HOST_URL : 'HOST_URL'
 }
